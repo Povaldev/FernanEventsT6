fc4fa619ea7dadda31fb83e9d6a629a97ef32313
pablo <pablovalderasdoblas.dev@gmail.com>
2026-03-05 21:59:38 +0100

    Creacion de setters y getters en evento y creacion del toStrign

--- src/Modelo/Evento.java
+++ src/Modelo/Evento.java
@@ -1,6 +1,7 @@
 package Modelo;
 
 import Modelo.Entradas.Entrada;
+import Vista.Vista;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -33,7 +34,128 @@ public class Evento {
     }
 
 
+    public static int getGeneraIDsEventos() {
+        return generaIDsEventos;
+    }
+
+    public static int getCantEventosCreados() {
+        return cantEventosCreados;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public int getAforo() {
+        return aforo;
+    }
+
+    public int getNumInscritos() {
+        return numInscritos;
+    }
+
+    public int getIdEvento() {
+        return idEvento;
+    }
+
+    public Entrada getEntrada() {
+        return entrada;
+    }
+
+    public static void setGeneraIDsEventos(int generaIDsEventos) {
+        Evento.generaIDsEventos = generaIDsEventos;
+    }
+
+    public static void setCantEventosCreados(int cantEventosCreados) {
+        Evento.cantEventosCreados = cantEventosCreados;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public void setAforo(int aforo) {
+        this.aforo = aforo;
+    }
+
+    public void setNumInscritos(int numInscritos) {
+        this.numInscritos = numInscritos;
+    }
+
+    public void setIdEvento(int idEvento) {
+        this.idEvento = idEvento;
+    }
+
+    public void setEntrada(Entrada entrada) {
+        this.entrada = entrada;
+    }
+
+    public String vistaDetalladaEvento() {
+        return Vista.vistaDetalladaEvento((double) (getNumInscritos() * 100) / getAforo());
+    }
+
+
+
+
+
+    public String muestraEventos(){
+
+    }
+
+
     public void creaEvento(){
 
+    }
+
+
+
+
+
+
+
+
+    public String toString() {
+        return "****(ID: " + this.idEvento + "****\n" +
+                "Nombre: " + this.nombre + "\n" +
+                "Descripcion: " + this.descripcion + "\n" +
+                "Categoría: " + this.categoria + "\n" +
+                "Fecha y hora: " + this.fecha + this.hora + "\n" +
+                "Aforo: " + this.aforo + "\n" +
+                "Nombre: " + this.nombre + "\n" +
+                vistaDetalladaEvento() + "\n" +
+
+                ;
     }
 }
